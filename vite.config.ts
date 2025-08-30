f4cc2920f147a01398bc4627377c274604cf8653
Prince Raj <praj12056@gmail.com>
2025-08-30 20:41:43 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/movie-revieww/",
   server: {
     host: "::",
     port: 8080,
